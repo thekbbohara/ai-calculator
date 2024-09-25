f940e031a5d642775ae811721b382f09d76ef960
kbzcraft <ytkbzcraft@gmail.com>
2024-09-25 22:09:19 +0545

    giving ans

--- src/app/api/img-to-ans/route.ts
+++ src/app/api/img-to-ans/route.ts
@@ -1,8 +1,9 @@
 // /app/api/route.ts
-
 import { NextRequest, NextResponse } from "next/server";
 import { GoogleAIFileManager } from "@google/generative-ai/server";
 import { writeFile } from "fs/promises";
+import { GoogleGenerativeAI } from "@google/generative-ai";
+import { prompt } from "@/lib/prompt";
 
 export async function POST(req: NextRequest) {
   const { img, mimeType, displayName } = await req.json();
@@ -15,17 +16,33 @@ export async function POST(req: NextRequest) {
   }
   const tmpFile = "/tmp/drawToCalc.jpg";
   await writeFile(tmpFile, img, "base64");
-
   const fileManager = new GoogleAIFileManager(API_KEY);
   try {
     const uploadResult = await fileManager.uploadFile(tmpFile, {
       mimeType,
       displayName,
     });
-    return NextResponse.json({
+    /* return NextResponse.json({
       message: `Uploaded file ${uploadResult.file.displayName}`,
       uri: uploadResult.file.uri,
-    });
+    }); */
+    if (uploadResult) {
+      const genAI = new GoogleGenerativeAI(API_KEY);
+      const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+      const result = await model.generateContent([
+        prompt(),
+        {
+          fileData: {
+            fileUri: uploadResult.file.uri,
+            mimeType: uploadResult.file.mimeType,
+          },
+        },
+      ]);
+      console.log(result.response.text());
+      return NextResponse.json({
+        message: result.response.text(),
+      });
+    }
   } catch (error) {
     return NextResponse.json(
       { message: "File upload failed", error: error },
